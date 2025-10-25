63009857b64f16725c43b5724c5a8d8d6242c4d8
Jon Salter <jmsalter@rocketmail.com>
2025-10-25 23:56:17 +0100

    next comit

--- server/src/reolvers.js
+++ server/src/reolvers.js
@@ -1,7 +1,15 @@
-const resolvers = {};
-Query: {
-  // returns an array of Tracks that will be used to populate
-  // the homepage grid of our web client
-  tracksForHome: () => {},
-}
+const resolvers = {
+  Query: {
+    // get all tracks, will be used to populate the homepage grid of our web client
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome(); 
+    },
+  Track: {
+    author: (parent, _, contextValue) => {
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
+    },
+  },
+};
+ 
 module.exports = resolvers;
