6aca7b20b8b261c45c7f358d329e39532f0a3ae4
Jon Salter <jmsalter@rocketmail.com>
2025-10-26 15:03:00 +0000

    Update 10

--- server/src/index.js
+++ server/src/index.js
@@ -1,32 +1,26 @@
-const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
-const typeDefs = require('./schema');
-
-
+ 
 async function startApolloServer() {
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-const { url } = await startStandaloneServer(server, {
-  context: async () => {
-    const { cache } = server;
-
-    return {
-      dataSources: {
-        trackAPI: new TrackAPI({ cache }),
-      },
-    };
-  },
-});
-
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+       
+      return {
+        dataSources: {
+          trackAPI: new TrackAPI({ cache }),
+        },
+      };
+    },
+  });
   console.log(`
-      🚀  Server is running
-      📭  Query at ${url}
-    `);
+    🚀  Server is running
+    📭  Query at ${url}
+  `);
 }
-
+ 
 startApolloServer();
